e0914716f14d10bba2bd153f80f72303a56f650a
jic <ji_camargo@yahoo.com>
2023-04-17 10:24:58 -0500

    add mobile menu

--- js/menumobile.js
+++ js/menumobile.js
@@ -1,44 +1,13 @@
+let burguer_menu = document.getElementById('burguer_menu');
+let	slideMenu = document.getElementById('slidemenu');
+let	elem_menu = document.querySelectorAll('#slidemenu a');
 
-	// objeto para propiedades
-	var propMenu ={
-		burguer_menu: document.getElementById('burguer_menu'),
-		slideMenu: document.getElementById('slidemenu'),
-	 	menu_activo: false,
-	 	elem_menu: document.querySelectorAll('#slidemenu .menu-principal a')
-	}
+function toggleMenu() {
+	slideMenu.classList.toggle("active");
+ }
 
+burguer_menu.addEventListener('click', toggleMenu); 
 
-	// objeto para metodos
-	var metMenu ={
-
-		inicio: function () {
-
-			propMenu.burguer_menu.addEventListener('click', metMenu.toggleMenu); 
-
-			for (var i = 0; i < propMenu.elem_menu.length; i++) {
-				
-				propMenu.elem_menu[i].addEventListener('click', metMenu.ocultarMenu) ;
-			}
-		},
-
-		toggleMenu: function () {
-
-			if (propMenu.menu_activo == false) {
-
-				propMenu.menu_activo = true ;
-				propMenu.slideMenu.className += ' active' ;
-			} else {
-				propMenu.menu_activo = false ;
-				propMenu.slideMenu.className = propMenu.slideMenu.className.replace('active', '');
-
-			}
-		},
-
-		ocultarMenu: function () {	
-				
-				propMenu.menu_activo = false ;
-				propMenu.slideMenu.className = propMenu.slideMenu.className.replace('active', '');
-		}
-	}
-
-	metMenu.inicio();
+for (let i = 0; i < elem_menu.length; i++) {
+		elem_menu[i].addEventListener('click', toggleMenu);
+}
