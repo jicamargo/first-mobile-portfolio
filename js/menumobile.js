c89e63ead19133cb6bd8bde843c43ac6da8b89c4
jic <ji_camargo@yahoo.com>
2023-04-17 12:28:18 -0500

    fix es linter error

--- js/menumobile.js
+++ js/menumobile.js
@@ -8,6 +8,7 @@ function toggleMenu() {
 
 menuBur.addEventListener('click', toggleMenu);
 
-for (let i = 0; i < elemMenu.length; i = i++) {
+for (let i = 0; i < elemMenu.length; i+=1) {
   elemMenu[i].addEventListener('click', toggleMenu);
 }
+
