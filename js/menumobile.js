870f1577d9ca18acdfbfcecfdacac4763377721d
jic <ji_camargo@yahoo.com>
2023-04-17 12:00:53 -0500

    fix CSS and ES linter error

--- js/menumobile.js
+++ js/menumobile.js
@@ -1,13 +1,13 @@
-const BurguerMenu = document.getElementById('BurguerMenu')
-const slideMenu = document.getElementById('slidemenu')
-const elemMenu = document.querySelectorAll('#slidemenu a')
+const BurguerMenu = document.getElementById('BurguerMenu');
+const slideMenu = document.getElementById('slidemenu');
+const elemMenu = document.querySelectorAll('#slidemenu a');
 
-function toggleMenu () {
-  slideMenu.classList.toggle('active')
+function toggleMenu() {
+  slideMenu.classList.toggle('active');
 }
 
-BurguerMenu.addEventListener('click', toggleMenu)
+BurguerMenu.addEventListener('click', toggleMenu);
 
 for (let i = 0; i < elemMenu.length; i + 1) {
-  elemMenu[i].addEventListener('click', toggleMenu)
+  elemMenu[i].addEventListener('click', toggleMenu);
 }
