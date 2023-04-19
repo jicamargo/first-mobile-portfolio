31e16e1c06a58a03bca4db61f61ca64806623b00
jic <ji_camargo@yahoo.com>
2023-04-18 22:03:02 -0500

    add links to live and souce btns in popup window

--- js/project-cards.js
+++ js/project-cards.js
@@ -156,8 +156,7 @@ for (let i = 0; i < buttons.length; i += 1) {
 /* this module creates a pop-up window with the information of the respective button's card clicked */
 
 function clickBtnMethod(event) {
-  
-  btn = event.srcElement;
+  let btn = event.srcElement;
   event.preventDefault();
   const indexProj  = parseInt(btn.id.slice(-1));
   popupWindow(arrWorks[indexProj]);
@@ -190,33 +189,49 @@ function popupWindow (card) {
     `<a id="btnClosePopup" class="btnClosePopup">
         <img class="btn-close-icon" src="images/close-icon.png" alt="Close menu icon.">
     </a> 
-      <h1 class="PopupTitle1 Title2_dark">${card.name}</h1>
-      <h2 class="PopupTitle2 Title2_card">${card.name2}</h2>
-      <ul class="list_feat_popup ">` +
+    <h1 class="PopupTitle1 Title2_dark">${card.name}</h1>
+    <h2 class="PopupTitle2 Title2_card">${card.name2}</h2>
+    <ul class="list_feat_popup ">` +
         card.technologies.map(tech => `
         <li class="li_btn_popup">${tech}</li>`).join(' ') + 
-      `</ul>
-      <div class="popupImgCnt">
+    `</ul>
+      
+    <div class="popup-columns">
+      <div class="popup-col1 popupImgCnt">
         <img class="PopupImg" src="${card.featureImage}" alt="${card.alternateTextImage}">
       </div>
-      <p class="PopupDescrip">${card.LongDescrip}</p>
-      <ul class="cntBtnPopup">
-        <li id="btn-live-${card.id}" class="SeeProjectBtn BtnClass btnSeeLive" href="${card.liveVersion}">
-          <div>See live</div>
-          <img class="icon-btn" src="./images/icon-btn-live.png"></li>
-        <li id="btn-src-${card.id}" class="SeeProjectBtn BtnClass btnSeeSource" href="${card.source}">
-          <div>See source</div>
-          <img class="icon-btn" src="./images/icon-btn-git.png"></li>
+      <div class="popup-col2">
+        <p class="PopupDescrip">${card.LongDescrip}</p>
+        <ul class="cntBtnPopup">
+          <li id="btnLive" class="SeeProjectBtn BtnClass btnSeeLive" href="${card.liveVersion}">
+            <div>See live</div>
+            <img class="icon-btn" src="./images/icon-btn-live.png"></li>
+          <li id="btnSrc" class="SeeProjectBtn BtnClass btnSeeSource" href="${card.source}">
+            <div>See source</div>
+            <img class="icon-btn" src="./images/icon-btn-git.png"></li>
         </ul>
+      </div>
+    </div>
     `;
 
   /* ********************************************** */
   
   popup_container.appendChild(div).setAttribute('id','modal');
+
   let BtnClose = document.getElementById('btnClosePopup');
+  let BtnLive = document.getElementById('btnLive');
+  let BtnSrc = document.getElementById('btnSrc');
+
   BtnClose.addEventListener('click', cerrarModal);
+  BtnLive.addEventListener('click', function() { visitSite(card.liveVersion)});
+  BtnSrc.addEventListener('click',  function() { visitSite(card.source)});
 }
 
 function cerrarModal () {
   cuerpoDom.removeChild(popup_container);
+}
+
+function visitSite(url) {
+  window.open(url);
+  cerrarModal();
 }
