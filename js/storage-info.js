987f8234860fec8dbcb830911acd72c1940e8efa
jic <ji_camargo@yahoo.com>
2023-04-20 09:57:16 -0500

    add storage-info.js to make new feature

---
+++ js/storage-info.js
@@ -0,0 +1,7 @@
+// When the user changes the any input field, the data is saved to the local storage.
+// When loads the page, if there is any data in the local storage the input fields are pre-filled with this data.
+
+let userInfo = {
+  name: ''
+}
+
