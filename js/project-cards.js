24d29f311bf96fe35877c18e3d7fe78ef78ed6f5
jic <ji_camargo@yahoo.com>
2023-09-25 16:45:10 -0500

    add projects to portfolio

--- js/project-cards.js
+++ js/project-cards.js
@@ -14,7 +14,7 @@ const arrWorks = [
                 + 'to make the most of their MMW experience. With an array of options, the MMW project serves as a one-stop destination, ensuring attendees stay informed and engaged throughout '
                 + 'the exciting music-filled weekend.',
     featureImage: './images/card1_img.png',
-    featureImage2: './images/snapshoot-mmw.png',
+    featureImage2: './images/snapshoot-project1.png',
     alternateTextImage: 'Miami Music Weekend project image.',
     technologies: ['CSS', 'HTML', 'JavaScript'],
     liveVersion: 'https://jicamargo.github.io/mmw/',
@@ -23,84 +23,87 @@ const arrWorks = [
 
   {
     id: 'project-1',
+    name: 'Journey Cash',
+    name2: 'Get Ready to Budget Like a Pro!',
+    ShortDescrip: "Effortlessly manage group finances with this powerful web app. Perfect for travel with friends,"+
+              " splitting bills, or coordinating group events, JourneyCash simplifies expense tracking and settlements.",
+    LongDescrip: 'JOURNEY CASH is a simple yet powerful expense tracking web application designed to help you manage your group finances effortlessly. ' +
+                "Whether you're traveling with friends, splitting bills with roommates, or organizing a group event, " +
+                "JourneyCash makes it easy to track expenses and settle up.",
+    featureImage: './images/card2_img.png',
+    featureImage2: './images/snapshoot-project2.png',
+    alternateTextImage: 'To Do list project image.',
+    technologies: ['Rails','Ruby','Postgres','CSS', 'HTML', 'JavaScript'],
+    liveVersion: 'https://journeycash.onrender.com/',
+    source: 'https://github.com/jicamargo/journeycash',
+  },
+  {
+    id: 'project-2',
+    name: 'Recipify',
+    name2: 'Cooking Made Simple with Recipify!',
+    ShortDescrip: "Your go-to app for simplifying cooking. Manage recipes, create shopping lists, and enjoy a better culinary experience, whether you're a pro or just starting out.",
+    LongDescrip: "Recipify is a Ruby on Rails web application designed to simplify your cooking experience. Whether you're an experienced chef or a novice in the kitchen, Recipify offers a user-friendly platform to manage your recipes, create shopping lists, and enhance your culinary journey.",
+    featureImage: '../images/card3_img.png',
+    featureImage2: '../images/snapshoot-project3.png',
+    alternateTextImage: 'secondary project ShortDescrip image.',
+    technologies: ['Rails','Ruby','Postgres','Bootstrap','CSS', 'HTML', 'JavaScript'],
+    liveVersion: 'https://recipify-eak3.onrender.com/',
+    source: 'https://github.com/jicamargo/recipify',
+  },
+  {
+    id: 'project-3',
+    name: 'That Sound',
+    name2: 'Unleash Sound Innovation!',
+    ShortDescrip: "Your ultimate sound library app, crafted with React and Redux. It's where innovation meets collaboration, offering dynamic user interfaces powered by React and state-of-the-art management through Redux, delivering an unmatched user experience.",
+    LongDescrip: "thatSound – a cutting-edge sound library application built using React and Redux. This repository serves as the central hub for collaborative development and innovation, where the power of React's dynamic user interfaces and Redux's state management combine to create an unparalleled user experience.",
+    featureImage: '../images/card4_img.png',
+    featureImage2: '../images/snapshoot-project4.png',
+    alternateTextImage: 'secondary project ShortDescrip image.',
+    technologies: ['React-JS', 'Redux', 'API','CSS', 'HTML', 'JavaScript'],
+    liveVersion: 'https://thatsound.netlify.app/',
+    source: 'https://github.com/jicamargo/thatSound',
+  },
+  {
+    id: 'project-4',
+    name: 'Space Travelers',
+    name2: 'Space Exploration at Your Fingertips!',
+    ShortDescrip: "Explore the Universe with Space-X-React-Group, a dynamic app created with React, Redux, CSS, and HTML. Discover the latest Space-X launches, rockets, and missions.",
+    LongDescrip: "A Space-X-React-Group app built with React, Redux, CSS, and HTML. Users can view information about the latest Space-X launches, rockets, and missions. The app also features a search function, allowing users to search for information about specific launches, rockets, or missions.",
+    featureImage: '../images/card5_img.png',
+    featureImage2: '../images/snapshoot-project5.png',
+    alternateTextImage: 'secondary project ShortDescrip image.',
+    technologies: ['React','Redux','CSS', 'HTML'],
+    liveVersion: 'https://space-travelers-five.vercel.app/',
+    source: 'https://github.com/jicamargo/space-travelers',
+  },
+  {
+    id: 'project-5',
+    name: 'BookStore',
+    name2: 'Your Books, Your Way!',
+    ShortDescrip: "The Bookstore is your go-to React app for effortlessly managing and showcasing your book collection, with features like adding, deleting, and viewing books.",
+    LongDescrip: "The Bookstore is a React application that allows users to manage and display a collection of books. It provides features such as adding new books, deleting existing books, and viewing the list of books.",
+    featureImage: '../images/card6_img.png',
+    featureImage2: '../images/snapshoot-project6.png',
+    alternateTextImage: 'secondary project ShortDescrip image.',
+    technologies: ['React','Webpack','CSS', 'HTML'],
+    liveVersion: 'https://bookstore-brn2h3xgr-jicamargo.vercel.app/',
+    source: 'https://github.com/jicamargo/bookstore',
+  },
+  {
+    id: 'project-6',
     name: 'To do list',
-    name2: 'Minimalist To Do List',
+    name2: 'Streamline Your Success!',
     ShortDescrip: 'A simple yet powerful tool that helps you stay organized and productive. With this app You can easily create tasks, edit, complete, and track your progress.',
     LongDescrip: 'A simple yet powerful tool that helps you stay organized and productive. With this app You can easily create tasks, edit, complete, and track your progress.'
     + '<br> This app offers several essential options to manage your tasks effectively. You can add new tasks by providing relevant details. The edit option allows you to modify '
     + 'existing tasks easily. Deleting a single task is simple, and if you have completed tasks cluttering your list, you can delete all completed tasks at once. The ability to '
     + "order tasks manually gives you control over their arrangement. Lastly, the project ensures that your data is preserved in local storage, so you won't lose any information even if you close or refresh the application.",
-    featureImage: './images/card2_img.png',
-    featureImage2: './images/snapshoot-todo.png',
+    featureImage: './images/card7_img.png',
+    featureImage2: './images/snapshoot-project7.png',
     alternateTextImage: 'To Do list project image.',
     technologies: ['CSS', 'HTML', 'JavaScript', 'Webpack'],
     liveVersion: 'https://jicamargo.github.io/todo/dist/',
     source: 'https://github.com/jicamargo/todo',
-  },
-  {
-    id: 'project-2',
-    name: 'Profesional Art 2',
-    name2: 'Printing Data',
-    ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    featureImage: '../images/img-ph1.png',
-    featureImage2: '../images/snapshoot-project1.png',
-    alternateTextImage: 'secondary project ShortDescrip image.',
-    technologies: ['CSS', 'Boostrap', 'Ruby'],
-    liveVersion: 'https://jicamargo.github.io/first-mobile-portfolio/',
-    source: 'https://github.com/jicamargo/first-mobile-portfolio',
-  },
-  {
-    id: 'project-3',
-    name: 'Profesional Art 3',
-    name2: 'Printing Data',
-    ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    featureImage: '../images/snapshoot-project1.png',
-    featureImage2: '../images/snapshoot-project1.png',
-    alternateTextImage: 'secondary project ShortDescrip image.',
-    technologies: ['CSS', 'Boostrap', 'Ruby'],
-    liveVersion: 'https://jicamargo.github.io/first-mobile-portfolio/',
-    source: 'https://github.com/jicamargo/first-mobile-portfolio',
-  },
-  {
-    id: 'project-4',
-    name: 'Profesional Art 4',
-    name2: 'Printing Data',
-    ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    featureImage: '../images/img-ph1.png',
-    featureImage2: '../images/snapshoot-project1.png',
-    alternateTextImage: 'secondary project ShortDescrip image.',
-    technologies: ['CSS', 'Boostrap', 'Ruby'],
-    liveVersion: 'https://jicamargo.github.io/first-mobile-portfolio/',
-    source: 'https://github.com/jicamargo/first-mobile-portfolio',
-  },
-  {
-    id: 'project-5',
-    name: 'Profesional Art 5',
-    name2: 'Printing Data',
-    ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    featureImage: '../images/img-ph1.png',
-    featureImage2: '../images/snapshoot-project1.png',
-    alternateTextImage: 'secondary project ShortDescrip image.',
-    technologies: ['CSS', 'Boostrap', 'Ruby'],
-    liveVersion: 'https://jicamargo.github.io/first-mobile-portfolio/',
-    source: 'https://github.com/jicamargo/first-mobile-portfolio',
-  },
-  {
-    id: 'project-6',
-    name: 'Profesional Art 6',
-    name2: 'Printing Data',
-    ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    featureImage: '../images/img-ph1.png',
-    featureImage2: '../images/snapshoot-project1.png',
-    alternateTextImage: 'secondary project ShortDescrip image.',
-    technologies: ['CSS', 'Boostrap', 'Ruby'],
-    liveVersion: 'https://jicamargo.github.io/first-mobile-portfolio/',
-    source: 'https://github.com/jicamargo/first-mobile-portfolio',
   },
 ];
 
@@ -120,42 +123,48 @@ for (let i = 0; i < arrWorks.length; i += 1) {
   if (i === 0) {
     div.classList = 'header1';
     div.id = card.id;
-    div.innerHTML = `<div class="cnt_img_main_card">
-       <img class="main_card_cnt_img" src="${card.featureImage}" alt="${card.alternateTextImage}">
-    </div>
-    <div class="cnt_info_main_card ">
-    <h3 class="MainCardTitle Title2_dark">${card.name}</h3>
-    <p class="MainCardDescrip">${card.ShortDescrip}</p>
-    <ul class="list_feat ">${
-  card.technologies.map((tech) => `
-      <li class="li_btn">${tech}</li>`).join(' ')
-}</ul>
-      <a id="btn-${card.id}" class="SeeProjectBtn BtnClass btnCard" href="#">See Project</a>
+    div.innerHTML = `
+      <div class="cnt_img_main_card">
+        <img class="main_card_cnt_img" src="${card.featureImage}" alt="${card.alternateTextImage}">
+      </div>
+      <div class="cnt_info_main_card ">
+        <div class="cnt_card_low_part">
+          <h3 class="MainCardTitle Title2_dark">${card.name}</h3>
+          <p class="MainCardDescrip">${card.ShortDescrip}</p>
+          <ul class="list_feat ">${
+              card.technologies.map((tech) => `
+              <li class="li_btn_2">${tech}</li>`).join(' ')
+            }
+          </ul>
+          <a id="btn-${card.id}" class="SeeProjectBtn project_btn_2 btnCard" href="#">See Project</a>
+        </div>
       </div>
       `;
   } else {
-    div.classList = `sec_card_cnt work-${i}`;
+    div.classList = `sec_card_cnt `;
     div.id = card.id;
     // div.style.background = (`url("${card.featureImage}") no-repeat center / cover`);
     div.innerHTML = `
-        <div class="info_card">
-        <div class="cnt_info_card">
-          <div class="cnt_img_card">
-            <img class="card_cnt_img" src="${card.featureImage}" alt="${card.alternateTextImage}">
+      <div class="info_card">
+        <div class="cnt_img_card">
+          <img class="card_cnt_img" src="${card.featureImage}" alt="${card.alternateTextImage}">
+        </div>
+        <div class="cnt_card_low_part">
+          <div class="cnt_info_card">
+            <h1 class="Title1_card">${card.name}</h1>
+            <h2 class="Title2_card">${card.name2}</h2>
+            <p class="CardDescrip">${card.ShortDescrip}</p>
+            <ul class="list_feat list_card_gral">${
+              card.technologies.map((tech) => `
+              <li class="li_btn_2">${tech}</li>`).join(' ')
+              }
+            </ul>
           </div>
-          <h1 class="Title1_card">${card.name}</h1>
-          <h2 class="Title2_card">${card.name2}</h2>
-          <p class="CardDescrip">${card.ShortDescrip}</p>
-          <ul class="list_feat list_card_gral">${
-            card.technologies.map((tech) => `
-            <li class="li_btn_2">${tech}</li>`).join(' ')
-            }
-          </ul>
+          <a id="btn-${card.id}" class="SeeProjectBtn project_btn_2 btnCard" href="#">See Project</a>
         </div> 
-        <a id="btn-${card.id}" class="SeeProjectBtn project_btn_2 btnCard" href="#">See Project</a>
-      </div>
-        `;
-  }
+    </div>
+  `;
+}
 
   gridWorks.appendChild(div);
 }
